8279921f798e71a5a0e2ab3be9bc780a17dd98a3
AD <142710797+adyson7@users.noreply.github.com>
2023-11-20 07:42:44 -0600

    Orbit React - push 1 - app does not run

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,34 @@
+// imports the React library and the satData module
+import React from "react";
+import satData from "./satData";
+
+// defines a function component: Buttons
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  // returns JSX (syntax extension for JavaScript) representing the component's UI (user interface)
   return (
+    // Container for the buttons
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {/* maps over the displaySats array to create buttons for each satellite type */}
+      {displaySats.map((sat, id) => (
+        // button for a specific satellite type, with an onClick event handler (function or method triggered by an event)
+        <button key={id} onClick={() => filterByType(sat)}>
+          {/* displays the satellite type in the button */}
+          {sat} Orbit
+        </button>
+      ))}
+
+      {/* button for displaying all satellite types */}
+      <button onClick={() => setSat([])}>All Orbits</button>
+
+      {/* button for filtering by Medium Orbit */}
+      <button onClick={() => filterByType("Medium")}>Medium Orbit</button>
+
+      {/* button for filtering by High Orbit */}
+      <button onClick={() => filterByType("High")}>High Orbit</button>
     </div>
   );
 };
 
+// exports the Buttons component for this module
 export default Buttons;
+
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,42 @@
+// imports the React library
+import React from "react";
+
+// defines a functional component: Table
 const Table = ({ sat }) => {
+  // returns JSX (syntax extension for JavaScript) representing the component's UI (user interface)
   return (
+    // Table element to display the satellite data
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        {/* Table header row with column titles */}
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* Map over the sat array to create rows for each satellite's data */}
+        {sat.map((data, id) => (
+          // Table row for a specific satellite, with a unique key
+          <tr key={id}>
+            {/* Table cells that contain satellite data */}
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            {/* displays "Active" if the satellite is operational, otherwise "Inactive" */}
+            <td>{data.operational ? "Active" : "Inactive"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
+//exports the Table component for this module
 export default Table;
+
+
+export default Table;
+
