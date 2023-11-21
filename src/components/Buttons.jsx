da3ffdf99dd1eb11544b5b7955f8d425d9d22173
AD <142710797+adyson7@users.noreply.github.com>
2023-11-20 18:57:38 -0600

    Final push- all tests passed

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -5,21 +5,15 @@ import satData from "./satData";
 function Buttons({ filterByType, setSat, displaySats }) {
   return (
     <div>
-      {/* Update the first button */}
+    //
       {displaySats.map((sat, id) => (
         <button onClick={() => filterByType(sat)} key={id}>
           {sat} Orbit
         </button>
       ))}
 
-      {/* Update the second button */}
       <button onClick={() => setSat(satData)}>All Orbits</button>
 
-      {/* Update the third button */}
-      <button onClick={() => filterByType("Medium")}>Medium Orbit</button>
-
-      {/* Update the fourth button */}
-      <button onClick={() => filterByType("High")}>High Orbit</button>
     </div>
   );
 }
